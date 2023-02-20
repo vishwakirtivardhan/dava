1d23db87ee786306f58d8a3b3e83e62384384f1c
vishwakirtivardhan <vishwakirtimaster@gmail.com>
2023-02-20 22:52:55 +0530

    New Module CMS created

---
+++ Modules/CMS/Routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::prefix('cms')->group(function() {
+//     Route::get('/', function () {
+//     return view('welcome');
+// });
+
+    // die(URL::current());
+    Route::get('/', 'CMSController@index');
+});
